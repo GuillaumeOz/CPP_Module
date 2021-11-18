3a5bc3c0d9d844729051b3626ecbc3262f47f6ef
Guillaume OZSERTTAS <gozsertt@e2r6p6.clusters.42paris.fr>
2021-11-18 11:36:53 +0100

    🐛 - Fix bug on cpp01 ex06

--- cpp01/ex06/srcs/class/Karen.cpp
+++ cpp01/ex06/srcs/class/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:05:40 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/11/17 19:55:39 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/11/18 11:35:57 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,34 @@ Karen::~Karen()
 
 void	Karen::debug()
 {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I just love it!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::info()
 {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money." << std::endl;
+	std::cout << "You don’t put enough!" << std::endl;
+	std::cout << "If you did I would not have to ask for it!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::warning()
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::error()
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::complain(std::string level)
