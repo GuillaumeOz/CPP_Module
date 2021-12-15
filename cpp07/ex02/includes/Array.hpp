7f0554e68a4c51eb131f96229fce0de49af1c33b
Guillaume OZSERTTAS <gozsertt@e2r13p9.clusters.42paris.fr>
2021-12-15 18:49:06 +0100

    🚧 - End cpp07, Start cpp08

--- cpp07/ex02/includes/Array.hpp
+++ cpp07/ex02/includes/Array.hpp
@@ -5,14 +5,82 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/14 20:14:15 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/12/14 20:20:43 by gozsertt         ###   ########.fr       */
+/*   Created: 2021/12/15 11:50:59 by gozsertt          #+#    #+#             */
+/*   Updated: 2021/12/15 16:16:55 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+template<typename T>
+class Array {
 
+	public:
+
+	Array<T>(void) : _size(0), _array(NULL) {
+
+		return ;
+	}
+
+	Array<T>(unsigned int n) : _size(n) {
+
+		if (n <= 0) {
+
+			this->_array = NULL;
+			throw std::range_error("Range Error");
+			return ;
+		}
+		this->_array = new T[n];
+		for (int i = 0; i < this->_size; i++)
+			this->_array[i] = 0;
+		return ;
+	}
+
+	Array<T>(Array<T> const & src) {
+
+		this->_size = 0;
+		(*this) = src;
+		return ;
+	}
+
+	~Array<T>(void) {
+
+		if (this->_size > 0)
+			delete [] this->_array;
+		return ;
+	}
+
+	Array<T> &operator=(Array<T> const & rhs) {
+
+		if (this == &rhs)
+			return (*this);
+		if (this->_size > 0)
+			delete [] this->_array;
+		this->_size = rhs._size;
+		this->_array = new T[rhs._size];
+		for (int i = 0; i < rhs._size; i++)
+			this->_array[i] = rhs._array[i];
+		return (*this);
+	}
+
+	T	&operator[](int const index) const {
+
+		if (index < 0 || index > this->_size || index == this->_size)
+			throw std::overflow_error("Overflow Error");
+		return (this->_array[index]);
+	}
+
+	int	size(void) const {
+
+		return (this->_size);
+	}
+
+	private:
+
+	int		_size;
+	T		*_array;
+	
+};
 
 #endif
--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 20:13:27 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/12/14 20:20:58 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/12/15 16:14:03 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,50 @@
 #define MAX_VAL 750
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = 0;
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = 42;
+	}
+	delete [] mirror;//
+	return 0;
 }
